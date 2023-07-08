998bdbde726609fb9396c538422bfd668c5c6d92
Michael Armstrong <armstrong.m@shaw.ca>
2023-07-07 22:53:04 -0600

    Updates

--- forms/contact.php
+++ forms/contact.php
@@ -4,7 +4,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'shawn@smncontracting.com';
+  $receiving_email_address = 'shawn.newell87@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
